232a71e3082113ec28bfa0cd331d20212a13abda
Andrew Fletcher <andrew@af-web.co.uk>
2017-02-13 21:27:59 +0000

    Adding initial Hex to Bin files.

---
+++ hex2bin/src/index.js
@@ -0,0 +1,22 @@
+var Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    // alexa.appId = 'amzn1.echo-sdk-ams.app.[unique-value-here]';
+
+    // alexa.dynamoDBTableName = 'YourTableNameUK'; // creates new table for session.attributes
+
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('MyIntent');
+    },
+
+    'MyIntent': function () {
+        this.emit(':tell', 'Hello World from Alexa');
+    }
+};
+
