b4f572170d1b0bb8ce8d4fe0c34639ced7b3901d
Andrew Fletcher <andrew@af-web.co.uk>
2017-02-14 01:19:40 +0000

    Working build ;-)

--- hex2bin/src/index.js
+++ hex2bin/src/index.js
@@ -14,18 +14,112 @@ const Alexa = require('alexa-sdk');
 
 const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
+const convertDigitToBinary = function(hex) {
+
+    switch (hex.toUpperCase()) {
+        case "0":
+            return "zero ,zero ,zero ,zero ";
+        case "1": 
+            return "zero ,zero ,zero ,one "; 
+        case "2": 
+            return "zero ,zero ,one ,zero ";
+        case "3": 
+            return "zero ,zero ,one ,one ";
+        case "4": 
+            return "zero ,one ,zero ,zero ";
+        case "5": 
+            return "zero ,one ,zero ,one ";
+        case "6": 
+            return "zero ,one ,one ,zero ";
+        case "7": 
+            return "zero ,one ,one ,one ";
+        case "8": 
+            return "one ,zero ,zero ,zero ";
+        case "9": 
+            return "one ,zero ,zero ,one ";
+        case "A": 
+            return "one ,zero ,one ,zero ";
+        case "B": 
+            return "one ,zero ,one ,one ";
+        case "C": 
+            return "one ,one ,zero ,zero ";
+        case "D": 
+            return "one ,one ,zero ,one ";
+        case "E": 
+            return "one ,one ,one ,zero ";
+        case "F": 
+            return "one ,one ,one ,one ";
+        default: 
+            return ""; 
+    }
+
+}
+
 const handlers = {
     "CaptureHexDigitIntent": function () {
 
         const readDigit = this.event.request.intent.slots.digit.value; 
 
-        this.emit(':ask', "The parsed digit is " + readDigit);
+        if (!readDigit) {
+            this.emit(':ask', "Please try again.");
+            return;
+        }
+
+        const binary = convertDigitToBinary(readDigit);
+
+        if (!this.attributes["curBin"]) {
+            this.attributes["curBin"] = " " + binary; 
+        }
+        else {
+            this.attributes["curBin"] = this.attributes["curBin"] + ", " + binary;
+        }
+
+        if (!this.attributes["curInput"]) {
+            this.attributes["curInput"] = readDigit; 
+        }
+        else {
+            this.attributes["curInput"] = this.attributes["curInput"] + ", " + readDigit;
+        }
+
+        this.emit(':ask', readDigit, "I didn't catch that. Please repeat that.");
     },
 
     "CompleteCaptureIntent": function () {
-        this.emit(':tell', 'You are done.');
+
+        var storedInput = this.attributes['curInput'];
+
+        this.attributes["canConfirm"] = true; 
+
+        this.emit(':ask', 'Thank you. I think you said: ' + storedInput + ". Is that correct?");
+
+    },
+    "ConfirmInput": function () {
+
+        if (!this.attributes["canConfirm"]) {
+            this.emit(':ask', "you can't confirm yet. What's your next digit?");
+            return;
+        }
+
+        this.attributes["canConfirm"] = false; 
+
+        var bin = this.attributes["curBin"]; 
+
+        var res = this.event.request.intent.slots.confirm.value;
+
+        this.attributes["curBin"] = null; 
+        this.attributes["curInput"] = null; 
+
+        if (res == "yes") {
+            this.emit(':tell', 'The binary number is: ' + bin);
+            this.attributes["capturing"] = false; 
+        }
+        else {
+            this.emit(':ask', 'Please start again!');   
+        }
+
     },
     "StartCaptureIntent": function () {
+
         this.emit(':ask', 'Speak your digits one after the other.');
     },
     "Unhandled": function() {
